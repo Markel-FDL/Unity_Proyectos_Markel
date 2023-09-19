6eef025ecaa57462c00e09d959cd7098fc66f60f
Markel <markel.fernandezdelabastida@ikasle.egibide.org>
2023-09-19 12:36:24 +0200

    Movimiento en los enemigos

--- Ejercicio_2_2D/Assets/Scripts/EnemyMovement.cs
+++ Ejercicio_2_2D/Assets/Scripts/EnemyMovement.cs
@@ -1,20 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyMovement : MonoBehaviour
 {
-    
-    
+    [SerializeField] private float velocidad;
+    [SerializeField] private float rango;
+    [SerializeField] private float distancia_Maxima;
+
+    [SerializeField]private Vector2 direccion;
     // Start is called before the first frame update
     void Start()
     {
-        
+        FijarNuevoDestino();
     }
 
     // Update is called once per frame
-    void Update()
+    // void Update()
+    // {
+    //     transform.position = Vector2.MoveTowards(transform.position, direccion, velocidad);
+    // }
+
+    private void FixedUpdate()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, direccion, velocidad);
+        if (Vector2.Distance(transform.position, direccion) < rango)
+        {
+            FijarNuevoDestino();
+        }
+    }
+    
+    void FijarNuevoDestino()
+    {
+        direccion = new Vector2(Random.Range(-distancia_Maxima, distancia_Maxima),
+            Random.Range(-distancia_Maxima, distancia_Maxima));
     }
 }
