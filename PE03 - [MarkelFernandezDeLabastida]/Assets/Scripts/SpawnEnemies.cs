2d593de1365ec57dcbfc12bf68b35031ceeeac52
Markel <markel.fernandezdelabastida@ikasle.egibide.org>
2023-10-17 17:48:18 +0200

    PE03 - (Attack of the blocks 3.0) Mejor

--- PE03 - [MarkelFernandezDeLabastida]/Assets/Scripts/SpawnEnemies.cs
+++ PE03 - [MarkelFernandezDeLabastida]/Assets/Scripts/SpawnEnemies.cs
@@ -14,10 +14,10 @@ public class Spawn_enemies : MonoBehaviour
     // Spawnea tres enemigos y un PowerUp en posiciones fijas del mapa.
     void Start()
     {
-        Instantiate(objeto, new Vector3(7f, -3), quaternion.identity);
-        Instantiate(objeto, new Vector3(-7f, 3f), quaternion.identity);
-        Instantiate(objeto, new Vector3(-6f, 0f), quaternion.identity);
-        Instantiate(powerUp, new Vector3(Random.Range(-7f, 7f), -3), quaternion.identity);
+        Instantiate(objeto, new Vector3(7f, -3.5f), quaternion.identity);
+        Instantiate(objeto, new Vector3(-7f, 3.5f), quaternion.identity);
+        Instantiate(objeto, new Vector3(-6.7f, 0f), quaternion.identity);
+        Instantiate(powerUp, new Vector3(Random.Range(-7.5f, 7.5f), -4f), quaternion.identity);
         Instantiate(vida, new Vector3(6f, -2f), quaternion.identity);
     }
 
